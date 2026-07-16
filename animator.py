2384e55d66c7aae2865d41632d46df8148d080c3
Sergei Fedorov <FedorovS@users.noreply.github.com>
2026-07-15 21:37:26 -0600

    Add animation offset interface

--- animator.py
+++ animator.py
@@ -10,3 +10,12 @@ class Animator:
     def update(self, dt):
 
         self.time += dt
+
+    def get_offset(self, layer_index):
+
+        if layer_index != 0:
+            return (0, 0)
+
+        x = math.sin(self.time * 2.0) * 10
+
+        return (x, 0)
